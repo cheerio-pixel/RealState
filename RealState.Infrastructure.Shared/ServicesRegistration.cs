db815ce4d3ceb6107d8ac70cb1bb815ef5f75bb8
JuanM-DS <20230676@itla.edu.do>
2024-07-22 17:46:58 -0400

    add emailSettings

--- RealState.Infrastructure.Shared/ServicesRegistration.cs
+++ RealState.Infrastructure.Shared/ServicesRegistration.cs
@@ -1,13 +1,20 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
+using RealState.Domain.Settings;
+
 namespace RealState.Infrastructure.Shared
 {
     public static class ServicesRegistration
     {
         public static void AddSharedLayer(this IServiceCollection services, IConfiguration configuration)
         {
+            #region Settings
+            services.Configure<EmailSettings>(provider => configuration.GetSection("EmailSettings").Bind(provider));
+            #endregion
 
+            #region
+            #endregion
         }
     }
 }
