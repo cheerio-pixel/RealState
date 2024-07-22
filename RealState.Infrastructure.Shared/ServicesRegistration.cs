9d511ea323a3525f37e32c468caba5f19315ca1f
JuanM-DS <20230676@itla.edu.do>
2024-07-22 17:32:55 -0400

    Add shared layer

---
+++ RealState.Infrastructure.Shared/ServicesRegistration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RealState.Infrastructure.Shared
+{
+    public static class ServicesRegistration
+    {
+        public static void AddSharedLayer(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
