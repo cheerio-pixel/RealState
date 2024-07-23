322322164a0787d8d2664fbe6e64b08a1f28cf2f
JuanM-DS <20230676@itla.edu.do>
2024-07-22 20:00:13 -0400

    Add IdentityDbContext

---
+++ RealState.Infrastructure.Identity/Context/IdentityContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RealState.Infrastructure.Identity.Context
+{
+    public class IdentityContext(DbContextOptions options) : IdentityDbContext(options)
+    {
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.HasDefaultSchema("Identity");
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ RealState.Infrastructure.Identity/Entities/ApplicationUser.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RealState.Infrastructure.Identity.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FirstName { get; set; } = null!;
+
+        public string LastName { get; set; } = null!;
+
+        public string Picture { get; set; } = null!;
+
+        public string IdentifierCard { get; set; } = null!;
+
+        // Navigators
+        public ICollection<ApplicationRole> Roles { get; set; } = [];
+    }
+}
