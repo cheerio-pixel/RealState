c59e45ef0e11a8a9fd40781edc0483b5259fb2e8
JuanM-DS <20230676@itla.edu.do>
2024-07-23 20:17:56 -0400

    Add Mappings profiles to identity

---
+++ RealState.Infrastructure.Identity/Mappings/GeneralIdentityProfiles.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+
+using RealState.Application.DTOs.Role;
+using RealState.Application.DTOs.User;
+using RealState.Infrastructure.Identity.Entities;
+
+namespace RealState.Infrastructure.Identity.Mappings
+{
+    public class GeneralIdentityProfiles : Profile
+    {
+        public GeneralIdentityProfiles()
+        {
+            #region User
+            CreateMap<ApplicationUser, ApplicationUserDTO>()
+                .ReverseMap();
+
+            CreateMap<ApplicationUser, SaveApplicationUserDTO>()
+                .ForMember(des => des.ConfirmPassword, opt => opt.Ignore())
+               .ReverseMap();
+            #endregion
+
+            #region Role
+            CreateMap<ApplicationRole, ApplicationRoleDTO>()
+               .ReverseMap();
+            #endregion
+        }
+    }
+}
