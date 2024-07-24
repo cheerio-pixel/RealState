c94b51e203989b0006384586e6ae7ca3bb9eda5d
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2024-07-23 21:31:50 -0400

    Add property type repository

---
+++ RealState.Application/Interfaces/Repositories/IPropertyTypeRepository.cs
@@ -0,0 +1,12 @@
+using RealState.Application.DTOs.PropertyType;
+using RealState.Application.QueryFilters.PropertyType;
+using RealState.Domain.Entities;
+
+namespace RealState.Application.Interfaces.Repositories
+{
+    public interface IPropertyTypeRepository
+    : IGenericRepository<PropertyTypes>
+    {
+        Task<List<PropertyTypeListItemDTO>> ListPropertyTypes(PropertyTypeQueryFilter filter);
+    }
+}
---
+++ RealState.Infrastructure.Persistence/Repositories/PropertyTypeRepository.cs
@@ -0,0 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+
+using RealState.Application.DTOs.PropertyType;
+using RealState.Application.Interfaces.Repositories;
+using RealState.Application.QueryFilters.PropertyType;
+using RealState.Domain.Entities;
+using RealState.Infrastructure.Persistence.Context;
+
+namespace RealState.Infrastructure.Persistence.Repositories
+{
+    internal class PropertyTypeRepository
+    : GenericRepository<PropertyTypes>, IPropertyTypeRepository
+    {
+        private readonly MainContext _context;
+
+        public PropertyTypeRepository(MainContext context) : base(context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<PropertyTypeListItemDTO>> ListPropertyTypes(PropertyTypeQueryFilter filter)
+        {
+            IQueryable<PropertyTypes> propertyTypeses = _context.PropertyTypes.AsQueryable();
+
+            if (!string.IsNullOrWhiteSpace(filter.Name))
+            {
+                propertyTypeses = propertyTypeses.Where(pt => pt.Name.Contains(filter.Name));
+            }
+
+            return await propertyTypeses
+                         .Select(pt => new PropertyTypeListItemDTO()
+                         {
+                             Name = pt.Name,
+                             Description = pt.Description,
+                             NumberOfProperties = pt.Properties.Count
+                         })
+                         .ToListAsync();
+        }
+    }
+}
