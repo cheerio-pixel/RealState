b70005436e6dd4b0d0da21540466f2d5c32762a5
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2024-07-24 12:00:26 -0400

    Re-Add Result object

---
+++ RealState.Application/Extras/ResultObject/AppError.cs
@@ -0,0 +1,13 @@
+
+namespace RealState.Application.Extras.ResultObject
+{
+    public record AppError(string PropertyName, string Message);
+
+    public static class AppErrorExt
+    {
+    }
+
+    public static class AppErrors
+    {
+    }
+}
