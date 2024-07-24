33af34cc7c6a5f969a81f4b829a1a5bf129d6254
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2024-07-23 21:47:24 -0400

    Add mappings for property type's types

---
+++ RealState.Application/Mapping/GeneralProfile.cs
@@ -0,0 +1,18 @@
+using AutoMapper;
+
+using RealState.Application.DTOs.PropertyType;
+using RealState.Application.ViewModel.PropertyType;
+using RealState.Domain.Entities;
+
+namespace RealState.Application.Mapping
+{
+    public class GeneralProfile : Profile
+    {
+        public GeneralProfile()
+        {
+            CreateMap<PropertyTypeViewModel, PropertyTypes>().ReverseMap();
+            CreateMap<PropertyTypeSaveViewModel, PropertyTypes>().ReverseMap();
+            CreateMap<PropertyTypeListItemDTO, PropertyTypeSaveViewModel>().ReverseMap();
+        }
+    }
+}
