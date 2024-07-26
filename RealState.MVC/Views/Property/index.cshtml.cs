bcd86475bb59429cd30b58a6cfe61414b0760e7c
MockAlan02 <alantubert09@gmail.com>
2024-07-25 23:04:57 -0400

    properties card

---
+++ RealState.MVC/Views/Property/index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RealState.MVC.Views.Property
+{
+    public class indexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
