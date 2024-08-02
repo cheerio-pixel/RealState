3f19319576d63cff26363c13c70f447f245cc4ee
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2024-08-01 20:52:20 -0400

    Register ErrorMiddleware

--- RealState.Api/Program.cs
+++ RealState.Api/Program.cs
@@ -1,6 +1,7 @@
 using RealState.Infrastructure.Persistence;
 using RealState.Infrastructure.Shared;
 using RealState.Infrastructure.Identity;
+using RealState.Api.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,7 +11,7 @@ builder.Services.AddIdentityLayer(builder.Configuration);
 builder.Services.AddSharedLayer(builder.Configuration);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
-builder.Services.AddControllers();
+builder.Services.AddControllers(static option => option.Filters.Add<ErrorMiddleware>());
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
