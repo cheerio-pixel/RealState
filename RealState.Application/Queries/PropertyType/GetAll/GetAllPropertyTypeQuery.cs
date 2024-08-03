328591f750957d8387890afe6a982f5077dbfb3e
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2024-08-03 10:49:50 -0400

    Add Swagger annotations to queries params

--- RealState.Application/Queries/PropertyType/GetAll/GetAllPropertyTypeQuery.cs
+++ RealState.Application/Queries/PropertyType/GetAll/GetAllPropertyTypeQuery.cs
@@ -3,11 +3,14 @@ using MediatR;
 using RealState.Application.DTOs.PropertyType;
 using RealState.Application.QueryFilters;
 
+using Swashbuckle.AspNetCore.Annotations;
+
 namespace RealState.Application.Queries.PropertyType.GetAll
 {
     public class GetAllPropertyTypeQuery
     : IRequest<List<PropertyTypeDTO>>
     {
+        [SwaggerParameter("Filter to apply to search")]
         public required PropertyTypeQueryFilter Filters { get; set; }
     }
 }
