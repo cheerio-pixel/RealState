099fb12021a1b675db20cccb3f89408cb2c6e56c
Frairlyn Camilo Roque Suarez <frailin300@gmail.com>
2024-08-03 13:36:31 -0400

    Add helper for ClaimsPrincipal

---
+++ RealState.MVC/Helpers/ClaimsPrincipalHelper.cs
@@ -0,0 +1,43 @@
+using System.Security.Claims;
+
+using RealState.Application.Enums;
+
+namespace RealState.MVC.Helpers
+{
+    internal static class ClaimsPrincipalHelper
+    {
+        /// <summary>
+        /// Return the id of the current logged user. Throws if non-existant
+        /// </summary>
+        public static Guid GetId(this ClaimsPrincipal self)
+        {
+            var guid = self.FindFirstValue(ClaimTypes.NameIdentifier);
+            ArgumentException.ThrowIfNullOrEmpty(guid);
+            return Guid.Parse(guid);
+        }
+
+        /// <summary>
+        /// Get the parsed Role of the current logged user. Throws if non-existant
+        /// </summary>
+        public static RoleTypes GetMainRole(this ClaimsPrincipal self)
+        {
+            string? role = self.FindFirstValue(ClaimTypes.Role);
+            ArgumentException.ThrowIfNullOrEmpty(role);
+            if (Enum.TryParse(role, out RoleTypes result))
+            {
+                return result;
+            }
+            throw new InvalidDataException(role);
+        }
+
+        /// <summary>
+        /// Get the Role of the current logged user. Throws if non-existant
+        /// </summary>
+        public static string GetUnparsedMainRole(this ClaimsPrincipal self)
+        {
+            string? role = self.FindFirstValue(ClaimTypes.Role);
+            ArgumentException.ThrowIfNullOrEmpty(role);
+            return role;
+        }
+    }
+}
