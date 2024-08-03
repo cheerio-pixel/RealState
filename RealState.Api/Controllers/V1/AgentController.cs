7ed82a18f8e80c6bca8ddb7c0f6d91d2dc630508
JuanM-DS <20230676@itla.edu.do>
2024-08-02 22:42:21 -0400

    Add change status endpoint and new validation

---
+++ RealState.Api/Controllers/V1/AgentController.cs
@@ -0,0 +1,30 @@
+﻿using Asp.Versioning;
+
+using MediatR;
+
+using Microsoft.AspNetCore.Mvc;
+
+using RealState.Application.Commands.Agent.ChangeStatus;
+
+namespace RealState.Api.Controllers.V1
+{
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiVersion("1.0")]
+    [ApiController]
+    public class AgentController : ControllerBase
+    {
+        private readonly ISender _sender;
+
+        public AgentController(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangeStatus(ChangeStatusAgentCommand cmd)
+        {
+            await _sender.Send(cmd);
+            return NoContent();
+        }
+    }
+}
--- RealState.Application/Commands/Agent/ChangeStatus/ChangeStatusAgentCommandHandler.cs
+++ RealState.Application/Commands/Agent/ChangeStatus/ChangeStatusAgentCommandHandler.cs
@@ -2,6 +2,7 @@
 
 using MediatR;
 
+using RealState.Application.Enums;
 using RealState.Application.Exceptions;
 using RealState.Application.Extras;
 using RealState.Application.Interfaces.Repositories;
@@ -20,17 +21,24 @@ namespace RealState.Application.Commands.Agent.ChangeStatus
 
         public async Task<Unit> Handle(ChangeStatusAgentCommand request, CancellationToken cancellationToken)
         {
-            var user = await _userRepository.Get(request.AgentId);
+            var user = await _userRepository.GetWithInclude(request.AgentId, ["Roles"]);
             if (user is null)
                 HttpStatusCode
                     .NoContent
                     .Because("There isn`t any user with that id")
                     .Throw();
 
+            var isAgent = user.Roles.Any(x=>x.Name == RoleTypes.StateAgent.ToString());
+            if (!isAgent)
+                HttpStatusCode
+                    .BadRequest
+                    .Because("This user isn`t an agent")
+                    .Throw();
+
             var result = await _userRepository.ChangeStatus(request.AgentId, request.Status);
             if(!result)
                 HttpStatusCode
-                    .NoContent
+                    .BadRequest
                     .Because("There is a problem while update status of agent")
                     .Throw();
 
